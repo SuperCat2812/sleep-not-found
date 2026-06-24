f9da4cbf05deb5bedf63ad3c486222e265a3a757
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-06-24 22:03:32 +0300

    validation of register form

--- components/RegistrationForm/RegistrationForm.tsx
+++ components/RegistrationForm/RegistrationForm.tsx
@@ -1,31 +1,80 @@
 "use client";
 // import css from "./RegistrationForm.module.css";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, FormikHelpers } from "formik";
+import Link from "next/link";
+import * as Yup from "yup";
+
 import { useId } from "react";
 
+interface RegisterFormValues {
+  name: string;
+  email: string;
+  password: string;
+}
+const initialValues: RegisterFormValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+const RegisterFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "The name must be at least 2 characters")
+    .max(20, "The name is too long")
+    .required("The name is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("The email is required"),
+  password: Yup.string().required(),
+});
 const RegistrationForm = () => {
   const fieldId = useId();
+  const handleSubmit = (
+    values: RegisterFormValues,
+    actions: FormikHelpers<RegisterFormValues>,
+  ) => {
+    /*register(values)  дані які ми передаємо у функцію для post на бекенд*/
+    console.log(values); //ВИДАЛИТИ ПОТІМ!!!
+    actions.resetForm();
+  };
   return (
     <>
       <h1>Реєстрація</h1>
-      <Formik initialValues={{}} onSubmit={() => {}}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={RegisterFormSchema}
+      >
         <Form>
           <label htmlFor={`${fieldId}-name`}>Ім&#39;я*</label>
-          <Field id={`${fieldId}-name`} type="text" name="name" placeholder='Ваше ім&#39;я'></Field>
+          <Field
+            id={`${fieldId}-name`}
+            type="text"
+            name="name"
+            placeholder="Ваше ім&#39;я"
+          ></Field>
 
           <label htmlFor={`${fieldId}-email`}>Пошта*</label>
-          <Field id={`${fieldId}-email`} type="email" name="email" placeholder='hello@leleka.com'></Field>
+          <Field
+            id={`${fieldId}-email`}
+            type="email"
+            name="email"
+            placeholder="hello@leleka.com"
+          ></Field>
 
           <label htmlFor={`${fieldId}-password`}>Пароль*</label>
           <Field
             id={`${fieldId}-password`}
             type="password"
             name="password"
-          placeholder='********'></Field>
+            placeholder="********"
+          ></Field>
 
           <button type="submit">Зареєструватись</button>
         </Form>
       </Formik>
+      <p>
+        Вже маєте аккаунт?<Link href={{}}>Увійти</Link>
+      </p>
     </>
   );
 };
