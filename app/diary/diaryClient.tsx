d4909ab4a30d7f63201bf123e0c7ff9af516f80f
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-06-29 10:45:18 +0200

    feat: implement DiaryClient component with diary entry functionality

---
+++ app/diary/diaryClient.tsx
@@ -0,0 +1,105 @@
+'use client';
+import DiaryList from '@/components/DiaryList/DiaryList';
+import css from './DiaryPage.module.css';
+import { useState } from 'react';
+import { DiaryNote, DiaryResponse } from '@/types/diary-types';
+import DiaryEntryDetails from '@/components/DiaryEntryDetails/DiaryEntryDetails';
+import Icon from '@/components/Icon/Icon';
+import { api } from '@/lib/api/api';
+import CustomScroll from '@/components/CustomScroll/CustomScroll';
+import AddDiaryEntryModal from '@/components/AddDiaryEntryModal/AddDiaryEntryModal';
+import GreetingBlock from '@/components/GreetingBlock/GreetingBlock';
+
+interface DiaryClientProps {
+  diarys: DiaryResponse;
+}
+
+const DiaryClient = ({ diarys }: DiaryClientProps) => {
+  const [id, setId] = useState<string>(diarys.diaryNotes[0]?._id ?? '');
+  const [data, setData] = useState<DiaryNote[]>(diarys.diaryNotes);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const loaderMore = async () => {
+    if (isLoading || page >= diarys.totalPages) return;
+    setIsLoading(true);
+    const nextPage = page + 1;
+    try {
+      const response = await api.get<DiaryResponse>('/diary', {
+        params: { page: nextPage, limit: 10 },
+      });
+      setData(prev => [...prev, ...response.data.diaryNotes]);
+      setPage(nextPage);
+    } catch {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSuccess = async () => {
+    try {
+      const response = await api.get<DiaryResponse>('/diary', {
+        params: { page: 1, limit: 10 },
+      });
+      setData(response.data.diaryNotes);
+      setPage(1);
+    } catch {}
+  };
+
+  const selectedDiary = data.find(diary => diary._id === id);
+
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const element = e.currentTarget;
+    if (
+      element.scrollTop + element.clientHeight >=
+      element.scrollHeight - 100
+    ) {
+      loaderMore();
+    }
+  };
+
+  return (
+    <>
+      <div className={css.Greeting}>
+        <GreetingBlock />
+      </div>
+      <section className={css.sectionDiary}>
+        <div className={css.diaryContainer}>
+          <div className={css.title}>
+            <h2>Ваші записи</h2>
+            <div className={css.createContainer}>
+              <button onClick={() => setIsModalOpen(true)}>
+                <span className={css.newTask}>Новий запис</span>
+                <Icon id="icon-plus" className={css.iconPlus} />
+              </button>
+            </div>
+          </div>
+          <div className={css.diaryContainer}>
+            <CustomScroll>
+              <div className={css.diaryListScroll} onScroll={handleScroll}>
+                <DiaryList diarys={data} setId={setId} />
+              </div>
+            </CustomScroll>
+          </div>
+        </div>
+        <div className={css.diaryContainerDetails}>
+          {selectedDiary && (
+            <DiaryEntryDetails
+              diary={selectedDiary}
+              onSuccess={handleSuccess}
+            />
+          )}
+        </div>
+        {isModalOpen && (
+          <AddDiaryEntryModal
+            onClose={() => setIsModalOpen(false)}
+            onSuccess={handleSuccess}
+          />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default DiaryClient;
