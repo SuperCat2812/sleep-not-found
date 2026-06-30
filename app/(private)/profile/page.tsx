57500cdcf172020e2ee2b42eb345bed1875bd535
vivtorochok <vika17052001@gmail.com>
2026-06-30 21:02:37 +0300

    RefreshProfile

--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -1,24 +1,33 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
-import ProfileAvatar from "@/components/ProfileAvatar/ProfileAvatar";
-import ProfileEditForm from "@/components/ProfileEditForm/ProfileEditForm";
-import { getMe } from "@/lib/api/clientApi";
-import { useAuthStore } from "@/lib/store/authStore";
-import css from "./ProfilePage.module.css";
+import ProfileAvatar from '@/components/ProfileAvatar/ProfileAvatar';
+import ProfileEditForm from '@/components/ProfileEditForm/ProfileEditForm';
+import Loader from '@/components/Loader/Loader';
+
+import { getMe } from '@/lib/api/clientApi';
+import { useAuthStore } from '@/lib/store/authStore';
+
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+import css from './ProfilePage.module.css';
 
 export default function ProfilePage() {
-  const setUser = useAuthStore((state) => state.setUser);
+  const setUser = useAuthStore(state => state.setUser);
 
   const {
     data: user,
     isLoading,
+    isFetching,
     isError,
   } = useQuery({
-    queryKey: ["currentUser"],
+    queryKey: ['currentUser'],
     queryFn: getMe,
+
     retry: false,
+    staleTime: 0,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
   });
 
   useEffect(() => {
@@ -27,12 +36,20 @@ export default function ProfilePage() {
     }
   }, [user, setUser]);
 
-  if (isLoading) {
-    return <p>Завантаження профілю...</p>;
+  if (isLoading || isFetching) {
+    return (
+      <section className={css.pageLoader}>
+        <Loader />
+      </section>
+    );
   }
 
   if (isError || !user) {
-    return <p>Не вдалося завантажити профіль</p>;
+    return (
+      <section className={css.page}>
+        <p>Не вдалося завантажити профіль</p>
+      </section>
+    );
   }
 
   return (
