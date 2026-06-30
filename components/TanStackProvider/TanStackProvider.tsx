9707e8c8dfe5b5ee6530073246b3c73a44723b54
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-06-30 14:22:08 +0200

    refactor: simplify TanStackProvider component and enhance user fetching logic

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,18 +1,36 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactNode, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getMe } from '@/lib/api/clientApi';
+import { useAuthStore } from '@/lib/store/authStore';
 
-interface TanStackProviderProps {
-  children: ReactNode;
-}
-
-const TanStackProvider = ({ children }: TanStackProviderProps) => {
+export default function TanStackProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [queryClient] = useState(() => new QueryClient());
+
+  const setUser = useAuthStore(state => state.setUser);
+  const clearIsAuthenticated = useAuthStore(
+    state => state.clearIsAuthenticated
+  );
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await getMe();
+        setUser(user);
+      } catch {
+        clearIsAuthenticated();
+      }
+    };
+
+    fetchUser();
+  }, [setUser, clearIsAuthenticated]);
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
-};
-
-export default TanStackProvider;
+}
