42fa9422a0d4885aa1952c348e20ebcc86cfd2cd
Ivan Alekseev <alekseeviva1992@gmail.com>
2026-06-30 10:37:52 +0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ const comfortaa = Comfortaa({
 export const metadata: Metadata = {
   title: 'Лелека',
   description: 'Персональний помічник для майбутніх мам',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
